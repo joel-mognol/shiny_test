fbc2e96f290f3ffe5465db3e889c8916dc4f436d
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2019-11-03 23:39:17 +0100

    Ajout de App06

---
+++ App06/app.R
@@ -0,0 +1,26 @@
+#Exemple issu de https://rstudio.github.io/DT/shiny.html
+library(shiny)
+library(DT)
+
+ui = fluidPage(
+  fluidRow(
+    column(6,textInput('prenom', "Choix du prénom :",
+                       placeholder = "EN MAJUSCULE")),
+    column(6,verbatimTextOutput('txt1', placeholder = TRUE))
+  ),
+  fluidRow(
+    column(6,DTOutput('tbl1')),
+    column(6,plotOutput("graph1"))
+  )
+)  
+server = function(input, output) {
+  choix <- reactive(filter(data,preusuel==input$prenom))
+  output$txt1 <- renderText(input$prenom)
+  output$tbl1 = renderDT(
+    choix(), options = list(lengthChange = FALSE)
+  )
+  output$graph1 <- renderPlot({
+    barplot(choix()$nombre,names.arg = choix()$annais,border = NA, main = paste("Occurence du prénom",input$prenom), xlab = "Années", ylab = "Nombre", las=2, cex.names=0.8)
+  })
+}
+shinyApp(ui,server)
