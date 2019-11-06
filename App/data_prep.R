e4ad543d7896962490f2ae1e39ea021e664e4288
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2019-11-06 23:10:14 +0100

    Nettoyage des tests et réorganisation des éléments

--- App/data_prep.R
+++ App/data_prep.R
@@ -12,13 +12,3 @@ nat2018 <- read_delim("App01/nat2018.csv",
                    trim_ws = TRUE)
 
 saveRDS(nat2018,file="prenoms.Rda")
-
-orions <- nat2018 %>%
-  filter(preusuel=="ORION")
-
-isiss <- nat2018 %>%
-  filter(preusuel=="ISIS")
-
-barplot(orions$nombre,names.arg = orions$annais,las=2)
-
-barplot(isiss$nombre,names.arg = isiss$annais,border = NA, main = "Occurence du prénom ISIS depuis 1968", xlab = "Années", ylab = "Nombre", las=2, cex.names=0.8)
