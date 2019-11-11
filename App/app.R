73d216d6379cb0f5688f690d9733394f9cd54506
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2019-11-11 23:43:54 +0100

    Nombreuses améliorations

--- App/app.R
+++ App/app.R
@@ -1,28 +1,33 @@
-#Exemple issu de https://rstudio.github.io/DT/shiny.html
 library(shiny)
 library(DT)
-#rm(data)
+
 data <- readRDS(file="../prenoms.Rda")
 
 ui = fluidPage(
+  titlePanel("Occurence des prénoms donnés à l'Etat civil français depuis 1900",
+             windowTitle = "Shiny Test"),
   fluidRow(
-    column(6,textInput('prenom', "Choix du prénom :",
-                       placeholder = "EN MAJUSCULE")),
-    column(6,verbatimTextOutput('txt1', placeholder = TRUE))
+    column(6,textInput('prenom', "Choix du prénom :",placeholder = "Ecrivez un prénom ici"))
   ),
   fluidRow(
-    column(6,DTOutput('tbl1')),
     column(6,plotOutput("graph1"))
+  ),
+  fluidRow(
+    column(6,DTOutput('tbl1'))
   )
-)  
+)
+
 server = function(input, output) {
-  choix <- reactive(filter(data,preusuel==input$prenom))
-  output$txt1 <- renderText(input$prenom)
-  output$tbl1 = renderDT(
-    choix(), options = list(lengthChange = FALSE)
-  )
-  output$graph1 <- renderPlot({
-    barplot(choix()$nombre,names.arg = choix()$annais,border = NA, main = paste("Occurence du prénom",input$prenom), xlab = "Années", ylab = "Nombre", las=2, cex.names=0.8)
-  })
+    choix <- reactive({
+      req(input$prenom)
+      filter(data,preusuel==toupper(input$prenom))
+      })
+    output$tbl1 = renderDT(choix(), options = list(lengthChange = FALSE)  )
+    output$graph1 <- renderPlot({
+      barplot(choix()$nombre,names.arg = choix()$annais,border = NA,
+              main = paste("Occurence du prénom",toupper(input$prenom)), xlab = "Années", ylab = "Nombre",
+              las=2, cex.names=0.8)
+    })
 }
+
 shinyApp(ui,server)
--- App/data_prep.R
+++ App/data_prep.R
@@ -1,14 +1,23 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+
 nat2018 <- read_delim("App/nat2018.csv",
                    ";",
                    escape_double = FALSE,
                    col_types = cols(
-                     sexe = col_integer(),
+                     sexe = col_character(),
                      preusuel = col_character(),
                      annais = col_integer(),
                      nombre = col_integer()),
-                   trim_ws = TRUE)
+                   trim_ws = TRUE) %>%
+  filter(!is.na(annais))
 
 saveRDS(nat2018,file="prenoms.Rda")
+
+nat2019 <- nat2018 %>%
+  pivot_wider(id_cols = c(preusuel,annais), names_from = sexe,
+              names_prefix = "sex_", values_from = nombre, values_fill = list(nombre=0)) %>%
+  rename(homme=sex_1,femme=sex_2)
+
+saveRDS(nat2019,file="prenoms2.Rda")
