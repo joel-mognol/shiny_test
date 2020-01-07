9591f1cbf55e21dc7b2cbab71a62222748fe9c96
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2020-01-07 23:06:05 +0100

    Diverses optimisations

--- App/app_dev.R
+++ App/app_dev.R
@@ -8,10 +8,8 @@ ui = fluidPage(
   titlePanel("Occurence des prénoms donnés à l'Etat civil français depuis 1900",
              windowTitle = "Shiny Test"),
   fluidRow(
-    column(6,textInput('prenom', "Choix du prénom :",placeholder = "Ecrivez un prénom ici"))
-  ),
-  fluidRow(
-    column(6,actionButton("go", "Afficher les statistiques"))
+    column(3,textInput('prenom', label = NULL,placeholder = "Ecrivez un prénom ici")),
+    column(3,actionButton("go", "Afficher les statistiques"))
   ),
   fluidRow(
     column(6,plotOutput("graph1"))
@@ -32,11 +30,10 @@ server = function(input, output) {
                 names_from = sexe,
                 names_prefix = "sex_",
                 values_from = nombre) %>%
-      rename("Année" = annais,Hommes=sex_1,Femmes=sex_2))
-  prenom_choisi <- eventReactive(input$go,{
-    req(input$prenom)
-    input$prenom})
-  output$graph1 <-renderPlot(print(ggplot(
+      rename("Année" = annais,Hommes=sex_1,Femmes=sex_2) %>%
+      select(-preusuel)
+    )
+  output$graph1 <-renderPlot(ggplot(
     data = choix(),
     mapping = aes(
       x = annais,
@@ -48,13 +45,15 @@ server = function(input, output) {
     coord_flip() +
     scale_y_continuous(labels = abs, limits = max(choix()$nombre) * c(-1,1)) +
     labs(x="Année de naissance",y = "Nombre") +
-    labs(title=paste("Répartition des personnes appelées",prenom_choisi()),
+    labs(title=paste("Répartition des personnes appelées",toupper(isolate(input$prenom))),
                      subtitle="Source : Insee d'après les déclarations à l'état civil français")
-    )
+    
     )
 
   output$tbl1 <- renderDT({ 
-    dat <- datatable(choixw(), options = list(paging=FALSE)) %>%
+    dat <- datatable(choixw(),
+                     options = list(paging=FALSE, searching = FALSE, info = FALSE),
+                     rownames = FALSE) %>%
       formatStyle('Hommes',  color = "#109CEF") %>%
       formatStyle('Femmes',  color = "#EF109C")
     return(dat)
