8ea58ef70ead96eb3650239594c55fc784647d1b
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2020-01-07 15:32:56 +0100

    Génération de nat2018l2 et substitution de barplot par ggplot

--- App/app_dev.R
+++ App/app_dev.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(DT)
+library(ggplot2)
 
-data <- readRDS(file="../prenoms.Rda")
+data <- readRDS(file="../prenoms_l2.Rda")
 
 ui = fluidPage(
   titlePanel("Occurence des prénoms donnés à l'Etat civil français depuis 1900",
@@ -19,28 +20,33 @@ ui = fluidPage(
     column(6,DTOutput('tbl1'))
   )
 )
-# randomVals <- eventReactive(input$go, {runif(input$n)})
 
-#  choix <- reactive({
-#    req(input$prenom)
-#    filter(data,preusuel==toupper(input$prenom))
-#   })
 
 server = function(input, output) {
   choix <- eventReactive(input$go,{
     req(input$prenom)
-    filter(data,preusuel==toupper(input$prenom))#à remplacer par un bloc de code issu de explorations.R
-    #Comment exécuter un bloc de code et affecter un résultat à choix en même temps
+    filter(data,preusuel==toupper(input$prenom))
     })
   prenom_choisi <- eventReactive(input$go,{
     req(input$prenom)
     input$prenom})
+  output$graph1 <-renderPlot(print(ggplot(
+    data = choix(),
+    mapping = aes(
+      x = annais,
+      y = ifelse(test = sexe == "1", yes = -nombre, no = nombre),
+      fill = sexe)) +
+    geom_col() +
+    scale_fill_manual(values = c("#109CEF", "#EF109C"),labels=c("Hommes", "Femmes")) +
+    theme(legend.position="bottom") +
+    coord_flip() +
+    scale_y_continuous(labels = abs, limits = max(choix()$nombre) * c(-1,1)) +
+    labs(x="Année de naissance",y = "Nombre") +
+    labs(title=paste("Répartition des personnes appelées",prenom_choisi()),
+                     subtitle="Source : Insee d'après les déclarations à l'état civil français")
+    )
+    )
   output$tbl1 <- renderDT(choix(), options = list(lengthChange = FALSE)  )
-  output$graph1 <- renderPlot({
-    barplot(choix()$nombre,names.arg = choix()$annais,border = NA,
-            main = paste("Occurence du prénom",toupper(prenom_choisi())), xlab = "Années",
-            ylab = "Nombre",las=2, cex.names=0.8)
-    })
 }
 
 shinyApp(ui,server)
--- App/data_prep.R
+++ App/data_prep.R
@@ -2,7 +2,8 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-#nat2018l avec l pour longer, désigne nat2018 avec les valeurs des deux sexes dans la même colonne
+#nat2018l avec l pour longer, désigne nat2018 avec
+#les valeurs des deux sexes dans la même colonne
 
 nat2018l <- read_delim("App/nat2018.csv",
                    ";",
@@ -15,13 +16,25 @@ nat2018l <- read_delim("App/nat2018.csv",
                    trim_ws = TRUE) %>%
   filter(!is.na(annais))
 
-saveRDS(nat2018l,file="prenoms.Rda")
+saveRDS(nat2018l,file="prenoms_l.Rda")
 
-#nat2018w avec w pour wider, désigne nat2018 avec les valeurs des deux sexes sur la même ligne
+#nat2018w avec w pour wider, désigne nat2018 avec
+#les valeurs des deux sexessur la même ligne
 
 nat2018w <- nat2018l %>%
   pivot_wider(id_cols = c(preusuel,annais), names_from = sexe,
               names_prefix = "sex_", values_from = nombre, values_fill = list(nombre=0)) %>%
   rename(homme=sex_1,femme=sex_2)
 
-saveRDS(nat2018w,file="prenoms2.Rda")
+saveRDS(nat2018w,file="prenoms_w.Rda")
+
+#nat2018l2 avec l pour longer, désigne nat2018 avec
+#les valeurs des deux sexes dans la même colonne et
+#des zéros dans les cas où il n'y a pas eu de naissance
+
+nat2018l2 <- nat2018w %>%
+  pivot_longer(cols = c(homme,femme), names_to = "sexe", values_to = "nombre") %>%
+  select(sexe,preusuel,annais,nombre)
+nat2018l2$sexe <- ifelse(nat2018l2$sexe == "homme", "1", "2")
+
+saveRDS(nat2018l2,file="prenoms_l2.Rda")
