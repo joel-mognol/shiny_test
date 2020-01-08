d7d826c9dba28e572c1f179273144ceb5a51a494
joel-mognol <34911806+joel-mognol@users.noreply.github.com>
2020-01-08 19:51:52 +0100

    Optimisation des pivots

--- App/app_dev.R
+++ App/app_dev.R
@@ -2,13 +2,13 @@ library(shiny)
 library(DT)
 library(ggplot2)
 
-data <- readRDS(file="../prenoms_l2.Rda")
+data <- readRDS(file="../prenoms_w.Rda")
 
 ui = fluidPage(
   titlePanel("Occurence des prénoms donnés à l'Etat civil français depuis 1900",
              windowTitle = "Shiny Test"),
   fluidRow(
-    column(3,textInput('prenom', label = NULL,placeholder = "Ecrivez un prénom ici")),
+    column(3,textInput('prenom', label = NULL, placeholder = "Ecrivez un prénom ici")),
     column(3,actionButton("go", "Afficher les statistiques"))
   ),
   fluidRow(
@@ -21,29 +21,29 @@ ui = fluidPage(
 
 
 server = function(input, output) {
-  choix <- eventReactive(input$go,{
+  choixw <- eventReactive(input$go, {
     req(input$prenom)
-    filter(data,preusuel==toupper(input$prenom))
-    })
-  choixw <- reactive(choix() %>%
-    pivot_wider(id_cols = c(preusuel,annais),
-                names_from = sexe,
-                names_prefix = "sex_",
-                values_from = nombre) %>%
-      rename("Année" = annais,Hommes=sex_1,Femmes=sex_2) %>%
+    filter(data, preusuel==toupper(input$prenom)) %>%
       select(-preusuel)
+    }
+    )
+  choixl <- reactive(choixw() %>%
+                        pivot_longer(cols = c(sex_1,sex_2),
+                                     names_to = "sexe",
+                                     values_to = "nombre") %>%
+                        select(annais,sexe,nombre)
     )
   output$graph1 <-renderPlot(ggplot(
-    data = choix(),
+    data = choixl(),
     mapping = aes(
       x = annais,
-      y = ifelse(test = sexe == "1", yes = -nombre, no = nombre),
+      y = ifelse(test = sexe == "sex_1", yes = -nombre, no = nombre),
       fill = sexe)) +
     geom_col() +
     scale_fill_manual(values = c("#109CEF", "#EF109C"),labels=c("Hommes", "Femmes")) +
     theme(legend.position="bottom") +
     coord_flip() +
-    scale_y_continuous(labels = abs, limits = max(choix()$nombre) * c(-1,1)) +
+    scale_y_continuous(labels = abs, limits = max(choixl()$nombre) * c(-1,1)) +
     labs(x="Année de naissance",y = "Nombre") +
     labs(title=paste("Répartition des personnes appelées",toupper(isolate(input$prenom))),
                      subtitle="Source : Insee d'après les déclarations à l'état civil français")
@@ -52,10 +52,13 @@ server = function(input, output) {
 
   output$tbl1 <- renderDT({ 
     dat <- datatable(choixw(),
-                     options = list(paging=FALSE, searching = FALSE, info = FALSE),
-                     rownames = FALSE) %>%
-      formatStyle('Hommes',  color = "#109CEF") %>%
-      formatStyle('Femmes',  color = "#EF109C")
+                     options = list(paging=FALSE,
+                                    searching = FALSE,
+                                    info = FALSE),
+                     rownames = FALSE,
+                     colnames = c('Année', 'Hommes', 'Femmes')) %>%
+      formatStyle('sex_1',  color = "#109CEF") %>%
+      formatStyle('sex_2',  color = "#EF109C")
     return(dat)
   })
 }
--- App/data_prep.R
+++ App/data_prep.R
@@ -2,10 +2,10 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-#nat2018l avec l pour longer, désigne nat2018 avec
-#les valeurs des deux sexes dans la même colonne
+#nat2018w avec w pour wider, désigne nat2018 avec
+#les valeurs des deux sexes sur la même ligne
 
-nat2018l <- read_delim("App/nat2018.csv",
+nat2018w <- read_delim("App/nat2018.csv",
                    ";",
                    escape_double = FALSE,
                    col_types = cols(
@@ -14,27 +14,11 @@ nat2018l <- read_delim("App/nat2018.csv",
                      annais = col_integer(),
                      nombre = col_integer()),
                    trim_ws = TRUE) %>%
-  filter(!is.na(annais))
-
-saveRDS(nat2018l,file="prenoms_l.Rda")
-
-#nat2018w avec w pour wider, désigne nat2018 avec
-#les valeurs des deux sexessur la même ligne
-
-nat2018w <- nat2018l %>%
-  pivot_wider(id_cols = c(preusuel,annais), names_from = sexe,
-              names_prefix = "sex_", values_from = nombre, values_fill = list(nombre=0)) %>%
-  rename(homme=sex_1,femme=sex_2)
+  filter(!is.na(annais)) %>%
+  pivot_wider(id_cols = c(preusuel,annais),
+              names_from = sexe,
+              names_prefix = "sex_",
+              values_from = nombre,
+              values_fill = list(nombre=0))
 
 saveRDS(nat2018w,file="prenoms_w.Rda")
-
-#nat2018l2 avec l pour longer, désigne nat2018 avec
-#les valeurs des deux sexes dans la même colonne et
-#des zéros dans les cas où il n'y a pas eu de naissance
-
-nat2018l2 <- nat2018w %>%
-  pivot_longer(cols = c(homme,femme), names_to = "sexe", values_to = "nombre") %>%
-  select(sexe,preusuel,annais,nombre)
-nat2018l2$sexe <- ifelse(nat2018l2$sexe == "homme", "1", "2")
-
-saveRDS(nat2018l2,file="prenoms_l2.Rda")
